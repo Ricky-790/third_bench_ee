03fe2381e330e39e2d0a3e3b8c04f74a81556bca
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-04-08 16:00:27 +0530

    Counters for Items Added

---
+++ javascript/script.js
@@ -0,0 +1,183 @@
+//initializing a variable name data
+let shoe_count = 0;
+let shock_count = 0;
+let shirt_count = 0;
+let pant_count = 0;
+let bag_count = 0;
+let jean_count = 0;
+let undergarment_count = 0;
+let jacket_count = 0;
+let hoodie_count = 0;
+let cap_count = 0;
+let glasses_count = 0;
+let sunglasses_count = 0;
+
+//For show_count
+document.getElementById("shoe_count_id").innerText = shoe_count;
+
+//creation of increment function
+function increment() {
+  shoe_count = shoe_count + 1;
+  document.getElementById("shoe_count_id").innerText = shoe_count;
+}
+//creation of decrement function
+function decrement() {
+  shoe_count = shoe_count - 1;
+  document.getElementById("shoe_count_id").innerText = shoe_count;
+}
+
+//For shock_count
+document.getElementById("shock_count_id").innerText = shock_count;
+
+//creation of increment function
+function increment() {
+  shock_count = shock_count + 1;
+  document.getElementById("shock_count_id").innerText = shock_count;
+}
+//creation of decrement function
+function decrement() {
+  shock_count = shock_count - 1;
+  document.getElementById("shock_count_id").innerText = shock_count;
+}
+
+//For shirt_count
+document.getElementById("shirt_count_id").innerText = shirt_count;
+
+//creation of increment function
+function increment() {
+  shirt_count = shirt_count + 1;
+  document.getElementById("shirt_count_id").innerText = shirt_count;
+}
+//creation of decrement function
+function decrement() {
+  shirt_count = shirt_count - 1;
+  document.getElementById("shirt_count_id").innerText = shirt_count;
+}
+
+//For pant_count
+document.getElementById("pant_count_id").innerText = pant_count;
+
+//creation of increment function
+function increment() {
+  pant_count = pant_count + 1;
+  document.getElementById("pant_count_id").innerText = pant_count;
+}
+//creation of decrement function
+function decrement() {
+  pant_count = pant_count - 1;
+  document.getElementById("pant_count_id").innerText = pant_count;
+}
+
+//For bag_count
+document.getElementById("bag_count_id").innerText = bag_count;
+
+//creation of increment function
+function increment() {
+  bag_count = bag_count + 1;
+  document.getElementById("bag_count_id").innerText = bag_count;
+}
+//creation of decrement function
+function decrement() {
+  bag_count = bag_count - 1;
+  document.getElementById("bag_count_id").innerText = bag_count;
+}
+
+//For jean_count
+document.getElementById("jean_count_id").innerText = jean_count;
+
+//creation of increment function
+function increment() {
+  jean_count = jean_count + 1;
+  document.getElementById("jean_count_id").innerText = jean_count;
+}
+//creation of decrement function
+function decrement() {
+  jean_count = jean_count - 1;
+  document.getElementById("jean_count_id").innerText = jean_count;
+}
+
+//For undergarment_count
+document.getElementById("undergarment_count_id").innerText = undergarment_count;
+
+//creation of increment function
+function increment() {
+  undergarment_count = undergarment_count + 1;
+  document.getElementById("undergarment_count_id").innerText =
+    undergarment_count;
+}
+//creation of decrement function
+function decrement() {
+  undergarment_count = undergarment_count - 1;
+  document.getElementById("undergarment_count_id").innerText =
+    undergarment_count;
+}
+
+//For jacket_count
+document.getElementById("jacket_count_id").innerText = jacket_count;
+
+//creation of increment function
+function increment() {
+  jacket_count = jacket_count + 1;
+  document.getElementById("jacket_count_id").innerText = jacket_count;
+}
+//creation of decrement function
+function decrement() {
+  jacket_count = jacket_count - 1;
+  document.getElementById("jacket_count_id").innerText = jacket_count;
+}
+
+//For hoodie_count
+document.getElementById("hoodie_count_id").innerText = hoodie_count;
+
+//creation of increment function
+function increment() {
+  hoodie_count = hoodie_count + 1;
+  document.getElementById("hoodie_count_id").innerText = hoodie_count;
+}
+//creation of decrement function
+function decrement() {
+  hoodie_count = hoodie_count - 1;
+  document.getElementById("hoodie_count_id").innerText = hoodie_count;
+}
+
+//For cap_count
+document.getElementById("cap_count_id").innerText = cap_count;
+
+//creation of increment function
+function increment() {
+  cap_count = cap_count + 1;
+  document.getElementById("cap_count_id").innerText = cap_count;
+}
+//creation of decrement function
+function decrement() {
+  cap_count = cap_count - 1;
+  document.getElementById("cap_count_id").innerText = cap_count;
+}
+
+//For glasses_count
+document.getElementById("glasses_count_id").innerText = glasses_count;
+
+//creation of increment function
+function increment() {
+  glasses_count = glasses_count + 1;
+  document.getElementById("glasses_count_id").innerText = glasses_count;
+}
+//creation of decrement function
+function decrement() {
+  glasses_count = glasses_count - 1;
+  document.getElementById("glasses_count_id").innerText = glasses_count;
+}
+
+//For sunglasses_count
+document.getElementById("sunglasses_count_id").innerText = sunglasses_count;
+
+//creation of increment function
+function increment() {
+  sunglasses_count = sunglasses_count + 1;
+  document.getElementById("sunglasses_count_id").innerText = sunglasses_count;
+}
+//creation of decrement function
+function decrement() {
+  sunglasses_count = sunglasses_count - 1;
+  document.getElementById("sunglasses_count_id").innerText = sunglasses_count;
+}
